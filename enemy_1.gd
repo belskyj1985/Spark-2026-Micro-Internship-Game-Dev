8c37451aded22ba1ab22f4434e524a045ab18838
JackGoDev <jackgo0325@gmail.com>
2026-02-09 11:31:31 -0600

    added barrel

--- enemy_1.gd
+++ enemy_1.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 const BulletScene = preload("res://enemy_1_bullet.tscn")
 @onready var range: Area2D = $range
 @onready var timer: Timer = $Timer
+@onready var barrel: Sprite2D = $Enemy1Barrel
 
 var active :bool = false
 var health :int = 100
@@ -17,9 +18,12 @@ func _on_range_body_entered(body: Node2D) -> void:
 func _on_range_body_exited(body: Node2D) -> void:
 	timer.stop()
 	active = false
-
+var tween 
+func _ready() -> void:
+	tween = get_tree().create_tween()
 
 func _on_timer_timeout() -> void:
+	tween.set_ease(Tween.EASE_OUT)
 	var bullet_instance: Bullet = BulletScene.instantiate()
 	get_tree().current_scene.add_child(bullet_instance)
 	var v = 600
@@ -34,4 +38,8 @@ func _on_timer_timeout() -> void:
 	#bullet_instance.setup(Global.player.global_position-global_position, 30)
 	bullet_instance.body_entered.connect(bullet_instance._on_body_entered)
 	bullet_instance.global_position = global_position
+	
+	tween.tween_property(barrel, "rotation",  Vector2(x_vel,-v).angle() + PI/2, 0.5)
+
+	barrel.rotation = Vector2(x_vel,-v).angle() + PI/2
 	print(x_vel)
