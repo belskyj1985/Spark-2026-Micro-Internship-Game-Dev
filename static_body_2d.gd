9267e1088eee10283b4777095a072321987d7304
belskyj1985 <belskyj1985@parkwayschools.net>
2026-02-11 13:20:12 -0600

    platforms can respawn with a variable

--- static_body_2d.gd
+++ static_body_2d.gd
@@ -4,6 +4,8 @@ extends StaticBody2D
 @onready var collapsing_platform: StaticBody2D = $"."
 @onready var timer_before_collapse: Timer = $TimerBeforeCollapse
 
+@export var respawning :bool = false
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is RigidBody2D:
 		wood_particles.emitting = true
@@ -17,8 +19,21 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_before_collapse_timeout() -> void:
-	wood_particles.emitting = true
-	
-	wood_particles.reparent(get_parent())
-	
-	collapsing_platform.queue_free()
+	if respawning == true:
+		
+		wood_particles.emitting = true
+		wood_particles.reparent(get_parent())
+		
+		visible = false
+		$Area2D/scanbox.set_deferred("disabled",true)
+		$TileMapLayer.collision_enabled = false
+		
+		await get_tree().create_timer(3.5).timeout
+		$TileMapLayer.collision_enabled = true
+		$Area2D/scanbox.set_deferred("disabled",false)
+		visible = true
+
+	else:
+		wood_particles.emitting = true
+		wood_particles.reparent(get_parent())
+		collapsing_platform.queue_free()
