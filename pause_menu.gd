51158d1e09923c17573110396f4c7b4bea3c2a04
JackGoDev <jackgo0325@gmail.com>
2026-03-02 21:06:22 -0600

    added health bar (placeholder graphic)

--- pause_menu.gd
+++ pause_menu.gd
@@ -9,6 +9,7 @@ func _on_recall_button_up() -> void:
 	down = false
 
 func _process(delta: float) -> void:
+
 	if Input.is_action_just_pressed("pause"):
 		print("PRESSED IT")
 		visible = !visible
--- ui.gd
+++ ui.gd
@@ -1,6 +1,15 @@
 extends Control
 @onready var c1: Label = $VBoxContainer/c1
 @onready var c2: Label = $VBoxContainer/c2
+@onready var health_bar: ProgressBar = $TextureProgressBar
+@onready var hp_text: Label = $TextureProgressBar/Label
 
+var hp_target :float = 100
 func _ready() -> void:
 	Global.UI = self
+
+func _physics_process(delta: float) -> void:
+	hp_target = float(Global.player.health)/Global.player.max_health * 100
+	if Global.player != null:
+		health_bar.value = lerp(health_bar.value, hp_target, 0.1)
+		hp_text.text = str(Global.player.health) + "/" + str(Global.player.max_health)
