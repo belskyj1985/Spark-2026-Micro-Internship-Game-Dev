c1728002ebf0dedce196adafc65cfbb59dea7a76
belskyj1985 <belskyj1985@parkwayschools.net>
2026-03-12 13:36:48 -0500

    Worked on level 2

--- physics_gear.gd
+++ physics_gear.gd
@@ -2,17 +2,26 @@ extends RigidBody2D
 
 @export var respawning :bool = false
 @onready var respawn_timer: Timer = $RespawnTimer
+@onready var spawn_timer: Timer = $SpawnTimer
 
 var starting_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-	if respawning:
-		starting_position = global_position
-		respawn_timer.start()
+	spawn_timer.start()
+	freeze = true
+
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	Global.player.get_hit(50)
 
+
+func _on_spawn_timer_timeout() -> void:
+	if respawning:
+		starting_position = global_position
+		respawn_timer.start()
+		freeze = false
+	else:
+		freeze = false
 
 func _on_respawn_timer_timeout() -> void:
 	global_position = starting_position
