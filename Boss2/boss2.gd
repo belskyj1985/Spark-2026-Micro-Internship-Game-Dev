504b0dc24ca11a3eff7439aa53f1a92a991878aa
JackGo <jackgo0325@gmail.com>
2026-03-24 14:26:10 -0500

    added boss shooting and jumping

--- Boss2/boss2.gd
+++ Boss2/boss2.gd
@@ -9,10 +9,12 @@ var action_count :int = 0
 var health :int = 3000
 var max_health :int = 3000
 var status: String = ""
-
+const acc :int = 1000
 var active :bool = true
-
+var bullet_offset = Vector2.ZERO
 var aggressive :bool = true
+var bullet_damage :int = 10
+const bullet = preload("res://Boss2/evil_player_bullet.tscn")
 
 func activate():
 	active = true
@@ -31,12 +33,13 @@ func get_player_side() -> int:
 	return sign(global_position.x - Global.player.global_position.x)
 
 func _on_action_timer_timeout() -> void:
+	print("ACTION")
 	if active:
 		match action_count%2:
 			1:
-				pass
+				action(1)
 			0:
-				pass
+				action(2)
 		#if global_position.distance_squared_to(Global.player.global_position) < 400:
 			#velocity = Vector2()
 		action_count += 1
@@ -46,6 +49,28 @@ var target_pos :Vector2 = Vector2(5150,290)
 
 var choice = randi_range(0,4)
 
+func shoot_angle():
+	for i in range(3):
+		var bullet_instance: Bullet = bullet.instantiate()
+		get_tree().current_scene.add_child(bullet_instance)
+		bullet_instance.setup(Global.player.global_position - (global_position + bullet_offset), bullet_damage)
+		bullet_instance.sprite.rotation = (get_global_mouse_position() - (global_position + bullet_offset)).angle() 
+		bullet_instance.global_position = global_position + bullet_offset
+		bullet_instance.speed = 400
+		await get_tree().create_timer(0.08).timeout
+
+func action(type :int = 1):
+	match type:
+		1:
+			if aggressive:
+				shoot_angle()
+			else:
+				shoot_angle()
+		2:
+			if aggressive:
+				shoot_angle() 
+			else:
+				shoot_angle()
 
 func _ready() -> void:
 	Global.boss = self
@@ -59,28 +84,43 @@ func _physics_process(delta: float) -> void:
 			velocity.y += 2000 * delta
 		
 		if aggressive:
+			if is_on_wall():
+				jump()
+			
 			if Global.player != null:
 				target_pos = Global.player.global_position + Vector2(50,0)
 			
-			if global_position.distance_squared_to(target_pos) < 100:
-				velocity.x = 0
+			if global_position.distance_squared_to(target_pos) < 200:
+				velocity.x = move_toward(velocity.x, 0, acc * delta)
 			else:
-				velocity.x = -sign(global_position.x - target_pos.x) * 200
+				velocity.x = move_toward(velocity.x, -sign(global_position.x - target_pos.x) * 200, acc * delta)
 		else:
 			if Global.player != null:
 				target_pos = Global.player.global_position + Vector2(200,0)
 			
-			if global_position.distance_squared_to(target_pos) < 100:
-				velocity.x = 0
+			if global_position.distance_squared_to(target_pos) < 200:
+				velocity.x = move_toward(velocity.x, 0, acc * delta)
 			else:
-				velocity.x = -sign(global_position.x - target_pos.x) * 200
+				velocity.x = move_toward(velocity.x, -sign(global_position.x - target_pos.x) * 200, acc * delta)
 	move_and_slide()
 
+func jump():
+	animator.play("jump")
+	velocity.y = -800
+
 func play_anims():
+	if sign(velocity.x) != 0:
+		sprite.flip_h = sign(velocity.x) == -1
+	if sprite.flip_h:
+		bullet_offset = Vector2(-37, -16)
+		sprite.offset.x = -6
+	else:
+		bullet_offset = Vector2(37, -16)
+		sprite.offset.x = 0
 	if is_on_floor():
 		if velocity.x == 0:
 			animator.play("stand")
-		else :
+		else:
 			animator.play("run")
 
 func apply_status(type):
